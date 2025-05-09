69cb114ef2de70e4c50abff0c235cd5053c3448c
zakhan14 <zuka.xantadze@gmail.com>
2025-05-09 16:13:30 +0200

    N4

--- nuevo_proyecto/nuevo_proyecto/wsgi.py
+++ nuevo_proyecto/nuevo_proyecto/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nuevo_proyecto.settings')
 
 application = get_wsgi_application()
+
+# Ruta a la carpeta de archivos estáticos recogidos por collectstatic
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
